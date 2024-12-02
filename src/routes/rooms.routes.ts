380802d8409ce434a1c14128361113ff4fe39bc5
DBS-Victor <victor.wollyver@dbssystem.com.br>
2024-12-01 21:30:22 -0300

    Feat: Adicinado conexão com o socket.io

--- src/routes/rooms.routes.ts
+++ src/routes/rooms.routes.ts
@@ -1,73 +1,83 @@
-import { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod";
 
 const CreateRoomSchema = z.object({
-  name: z
-    .string({ message: "O nome da sala é obrigatório" })
-    .min(1, { message: "Precisa ter pelo menos 1 caracter" })
-    .max(20, { message: "Precisa ter pelo menos de 20 caracteres" }),
-  password: z.string().optional().nullable(),
-  privacyRoom: z.enum(["PUBLIC", "PRIVATE"], {
-    message: "A propriedade 'privacyRoom' precisa ser 'PUBLIC' ou 'PRIVATE'",
-  }),
+	name: z.string({ message: "O nome da sala é obrigatório" }).min(1, { message: "Precisa ter pelo menos 1 caracter" }).max(20, { message: "Precisa ter pelo menos de 20 caracteres" }),
+	password: z.string().optional().nullable(),
+	privacyRoom: z.enum(["PUBLIC", "PRIVATE"], {
+		message: "A propriedade 'privacyRoom' precisa ser 'PUBLIC' ou 'PRIVATE'",
+	}),
 });
 
 type CreateRouteType = z.infer<typeof CreateRoomSchema>;
 
 export async function rooms(app: FastifyInstance) {
-  app.get("/", async (req, res) => {
-    try {
-      const response = await prisma.rooms.findMany();
+	app.get("/", async (req, res) => {
+		try {
+			const response = await prisma.rooms.findMany();
 
-      res.send({ message: "Sucesso ao buscar salas!", data: response });
-    } catch (error: any) {
-      res.status(500).send({
-        message: "Erro inesperado ao buscar salas!",
-        error: error.message,
-      });
-      console.error(error);
-    }
-  });
+			res.send({ message: "Sucesso ao buscar salas!", data: response });
+		} catch (error: any) {
+			res.status(500).send({
+				message: "Erro inesperado ao buscar salas!",
+				error: error.message,
+			});
+			console.error(error);
+		}
+	});
 
-  app.post("/create", async (req, res) => {
-    const { name, password, privacyRoom } = req.body as CreateRouteType;
+	app.post("/create", async (req, res) => {
+		const { name, password, privacyRoom } = req.body as CreateRouteType;
 
-    try {
-      CreateRoomSchema.parse(req.body);
+		try {
+			CreateRoomSchema.parse(req.body);
 
-      if (privacyRoom === "PRIVATE" && !password) {
-        throw new Error("É necessário informar a senha!");
-      }
+			if (privacyRoom === "PRIVATE" && !password) {
+				throw new Error("É necessário informar a senha!");
+			}
 
-      const response = await prisma.rooms.create({
-        data: {
-          name: name,
-          password: privacyRoom === "PRIVATE" ? password : "",
-          privacyRoom: privacyRoom,
-        },
-      });
+			const response = await prisma.rooms.create({
+				data: {
+					name: name,
+					password: privacyRoom === "PRIVATE" ? password : "",
+					privacyRoom: privacyRoom,
+				},
+			});
 
-      res.send({
-        message: "Sala criada com sucesso!",
-        data: response,
-      });
-    } catch (error: unknown) {
-      if (error instanceof z.ZodError) {
-        res.status(400).send({ message: error.errors[0].message });
-      }
+			res.send({
+				message: "Sala criada com sucesso!",
+				data: response,
+			});
+		} catch (error: unknown) {
+			if (error instanceof z.ZodError) {
+				res.status(400).send({ message: error.errors[0].message });
+			}
 
-      if (error instanceof Error) {
-        res.status(400).send({
-          message: error.message,
-        });
-      }
+			if (error instanceof Error) {
+				res.status(400).send({
+					message: error.message,
+				});
+			}
 
-      res
-        .status(500)
-        .send({ message: "Erro inesperado ao criar sala!", error: error });
+			res.status(500).send({ message: "Erro inesperado ao criar sala!", error: error });
 
-      console.error(error);
-    }
-  });
+			console.error(error);
+		}
+	});
+
+	app.get("/getRoomById/:id", async (req, res) => {
+		const { id } = req.params as { id: string };
+
+		try {
+			const response = await prisma.rooms.findUnique({ where: { id: id } });
+
+			res.send({
+				message: "Sala encontrada!",
+				data: response,
+			});
+		} catch (error: unknown) {
+			res.status(500).send({ message: "Erro inesperado ao criar sala!", error: error });
+		}
+	});
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,44 @@
 import { fastify } from "fastify";
+import fastifyIO from "fastify-socket.io";
 
 import { rooms } from "./routes";
 
 const app = fastify();
 
+app.register(fastifyIO, {
+	cors: {
+		origin: "*",
+	},
+});
+
 app.register(rooms, { prefix: "/rooms" });
 
+app.ready().then(() => {
+	console.log("app ready");
+	app.io.on("connection", (socket) => {
+		let count = 0;
+		console.log("socket connected", socket.id);
+
+		socket.on("join-room", (roomId) => {
+			socket.join(roomId);
+			console.log("joined room", roomId);
+		});
+
+		socket.on("click", (countClient) => {
+			count++;
+			console.log("O scoket: ", socket.id, " clicou ", countClient, " vezes");
+			console.log("O total de cliques é: ", count);
+			console.log("O total de cliques client é: ", countClient);
+		});
+
+		socket.on("disconnect", () => {
+			console.log("socket disconnected", socket.id);
+		});
+	});
+});
+
 const PORT = Number(process.env.PORT);
+
 app.listen({ port: PORT }).then(() => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
